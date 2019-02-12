1658998b1e4b5136860aa24ac414ac0a3f308193
prashanth-g <prashanthcsgce@gmail.com>
2019-02-12 20:10:25 +0530

    Added SpringGCPApplication boot class

---
+++ src/main/java/com/prashanth/spring/gcloud/SpringGCPApplication.java
@@ -0,0 +1,11 @@
+package com.prashanth.spring.gcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGCPApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGCPApplication.class, args);
+    }
+}
