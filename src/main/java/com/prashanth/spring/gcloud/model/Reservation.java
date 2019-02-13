63ac0befcbacda985ba94bf82276a614e7c8afa8
prashanth-g <prashanthcsgce@gmail.com>
2019-02-13 22:11:38 +0530

    Added Reservation Entity

---
+++ src/main/java/com/prashanth/spring/gcloud/model/Reservation.java
@@ -0,0 +1,13 @@
+package com.prashanth.spring.gcloud.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Reservation {
+    private int id;
+    private String name;
+}
