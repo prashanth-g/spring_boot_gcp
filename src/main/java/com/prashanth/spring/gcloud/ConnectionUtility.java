2d3e29b3ab7874fbaff8b8a1f967a8bf85c8194d
prashanth-g <prashanthcsgce@gmail.com>
2019-02-15 17:12:10 +0530

    Updated ConnectionUtility with jdbc template

--- src/main/java/com/prashanth/spring/gcloud/ConnectionUtility.java
+++ src/main/java/com/prashanth/spring/gcloud/ConnectionUtility.java
@@ -1,11 +1,17 @@
 package com.prashanth.spring.gcloud;
 
+import com.prashanth.spring.gcloud.model.Reservation;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
+@Log4j2
 public class ConnectionUtility {
 
     private final JdbcTemplate jdbcTemplate;
@@ -15,5 +21,10 @@ public class ConnectionUtility {
     }
 
     @EventListener(ApplicationReadyEvent.class)
-    public void launch() throws Exception {}
+    public void launch() throws Exception {
+        List<Reservation> reservationList = this.jdbcTemplate.query("select * from reservation",
+                (rs, rowNumber) -> new Reservation(rs.getInt("id"), rs.getString("name")));
+        reservationList.forEach(log::info);
+    }
 }
+
