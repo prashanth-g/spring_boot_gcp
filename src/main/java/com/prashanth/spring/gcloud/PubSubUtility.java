b808b1d1bd2a0a401ca22379cf0cf96e366efb70
prashanth-g <prashanthcsgce@gmail.com>
2019-02-21 21:58:26 +0530

    Updated PubSubUtility

--- src/main/java/com/prashanth/spring/gcloud/PubSubUtility.java
+++ src/main/java/com/prashanth/spring/gcloud/PubSubUtility.java
@@ -1,27 +1,42 @@
 package com.prashanth.spring.gcloud;
 
+import com.google.protobuf.ByteString;
 import com.prashanth.spring.gcloud.model.SpannerReservation;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.cloud.gcp.pubsub.core.publisher.PubSubPublisherTemplate;
 import org.springframework.cloud.gcp.pubsub.core.subscriber.PubSubSubscriberTemplate;
+import org.springframework.cloud.gcp.pubsub.support.BasicAcknowledgeablePubsubMessage;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+import java.time.Instant;
+import java.util.function.Consumer;
 import java.util.stream.Stream;
 
 @Component
+@Log4j2
 public class PubSubUtility {
 
-    private final PubSubPublisherTemplate pubSubPublisherTemplate;
-    private final PubSubSubscriberTemplate pubSubSubscriberTemplate;
+    private final PubSubPublisherTemplate publisher;
+    private final PubSubSubscriberTemplate subscriber;
 
-    PubSubUtility(PubSubPublisherTemplate pubSubPublisherTemplate, PubSubSubscriberTemplate pubSubSubscriberTemplate) {
-        this.pubSubPublisherTemplate = pubSubPublisherTemplate;
-        this.pubSubSubscriberTemplate = pubSubSubscriberTemplate;
+    PubSubUtility(PubSubPublisherTemplate publisher, PubSubSubscriberTemplate subscriber) {
+        this.publisher = publisher;
+        this.subscriber = subscriber;
     }
 
     @EventListener(ApplicationReadyEvent.class)
     public void launch() throws Exception {
+        this.subscriber.subscribe("reservations-subscription", new Consumer<BasicAcknowledgeablePubsubMessage>() {
+            @Override
+            public void accept(BasicAcknowledgeablePubsubMessage msg) {
+                String data =  msg.getPubsubMessage().getData().toStringUtf8();
+                log.info("message received: " + data);
+                msg.ack();
+            }
+        });
 
+        this.publisher.publish("reservations", "Hola " + Instant.now().toString());
     }
 }
